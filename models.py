839a80dc320d89c0cfc5456769e818b016ffddaa
artem <artem.gusev2004@bk.ru>
2024-05-15 02:46:48 +0400

    sddfsefsef

--- models.py
+++ models.py
@@ -7,7 +7,10 @@ class Museum(pw.Model):
     title = pw.CharField(max_length=100)
     description = pw.TextField()
     image = pw.CharField()
-
+    contacts = pw.TextField()
+    website = pw.TextField()
+    address = pw.TextField()
+    work_time = pw.TextField()
     class Meta:
         database = db
 
