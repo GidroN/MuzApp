fe03ecc8230def902d2ce1fe9d16fb809d0c8830
artem <artem.gusev2004@bk.ru>
2024-06-08 11:18:51 +0300

    Перекинул ROUTES из файла events в файл routesevents

--- events.py
+++ events.py
@@ -1,14 +1,8 @@
 from flet_core import ListView
 import flet as ft
-
+from  routesevents import ROUTES
 # from flet_map import FletMap
 
-
-ROUTES = {
-    0: '/about_app/',
-    1: '/about_city/',
-    2: '/events/',
-}
 
 
 def close_click(e):
@@ -31,3 +25,27 @@ def change_route(e):
     route = ROUTES[e.control.selected_index]
     e.page.go(route)
     print(route)
+
+def appbar(e, text):
+   return ft.AppBar(
+        leading=ft.IconButton(ft.icons.MUSEUM, on_click=show_drawer),
+        leading_width=40,
+        title=ft.Text(text),
+        center_title=False,
+        bgcolor=ft.colors.SURFACE_VARIANT, actions=[
+            ft.IconButton(ft.icons.WB_SUNNY_OUTLINED, on_click=change_theme),
+            ft.PopupMenuButton(
+                items=[
+                    ft.PopupMenuItem(
+                        "Настройки", on_click=lambda _: e.page.go('/settings/')
+                    ),
+                    ft.PopupMenuItem(
+                        text="Поддержка", on_click=...
+                    ),
+                    ft.PopupMenuItem(
+                        text="Выйти", on_click=close_click
+                    ),
+                ]
+            ),
+        ],
+    ),
