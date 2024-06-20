5d414f8bc8983a7f02d049f497c23570c2d84e95
artem <artem.gusev2004@bk.ru>
2024-06-21 01:08:06 +0300

    це кайф

--- migrate.py
+++ migrate.py
@@ -1,9 +1,16 @@
 from playhouse.migrate import *
+from peewee import SqliteDatabase, ForeignKeyField
+from models import MapCoordinates
+
+# Initialize the database
 my_db = SqliteDatabase('db.sqlite3')
 migrator = SqliteMigrator(my_db)
 
-work_time = TextField(null=True)
+# Define the ForeignKeyField with the correct field reference
+coordinates = ForeignKeyField(MapCoordinates, field=MapCoordinates.id, backref='museum', default=1)
 
+# Perform the migration to add the new column
 migrate(
-    migrator.add_column('museum', 'work_time',work_time),
+    migrator.add_column('museum', 'coordinates_id', coordinates)
 )
+
